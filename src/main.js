a366b5c86034ae602e9059050b30e50e3cdd84f1
怡瑄 <a6687225@gmail.com>
2023-04-24 03:31:06 +0800

    meta
    meta

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ShareNetwork from 'vue-social-sharing'
-import { createMetaManager } from 'vue-meta'
 import "./index.css";
 
 const app = createApp(App)
-// app.use(createMetaManager(false, {
-//     meta: { tag: 'meta', nameless: true }
-// }))
+
 app.use(router)
 app.use(ShareNetwork)
-app.use(createMetaManager())
 app.mount('#app')
