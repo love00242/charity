5f19722e2a13e42a035b07fd79550e1b1b56defe
怡瑄 <a6687225@gmail.com>
2023-04-24 03:00:17 +0800

    meta
    meta

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterView, useRoute } from 'vue-router';
+import { useMeta } from 'vue-meta';
 import { debounce } from '@/utils/common';
 import { ref, provide, onMounted, computed } from 'vue';
- 
+
 const route = useRoute()
- 
+
 const routerKey = computed(() => {
   return route.path + Math.random();
 })
@@ -17,6 +18,18 @@ onMounted(() => {
   window.addEventListener('resize', debounce(() => {
     isPC.value = window.innerWidth >= 1024;
   }));
+  useMeta({
+    meta: [
+      {
+        name: 'keywords',
+        content: "測試"
+      },
+      {
+        property: "description",
+        content: "測試測試"
+      },
+    ]
+  })
 });
 </script>
 
