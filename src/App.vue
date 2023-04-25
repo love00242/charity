7460e337e0e13e7572f62c526122a423ead8b446
怡瑄 <a6687225@gmail.com>
2023-04-25 21:47:17 +0800

    og:url
    og:url

--- src/App.vue
+++ src/App.vue
@@ -18,30 +18,33 @@ provide("isPC", isPC);
 watch(() => route.path, () => {
   console.log("route.path", route.path, url, articleList[0].title);
 
-  document
-    .querySelector('meta[property="og:url"]')
-    .setAttribute("content", url);
   if (route.path.includes("articledetail")) {
-    // document
-    //   .querySelector('meta[property="og:url"]')
-    //   .setAttribute("content", url);
     const articleNum = route?.query.article;
+    const title = articleList[articleNum - 1].title;
+    document
+      .querySelector('meta[property="og:url"]')
+      .setAttribute("content", `https://charity-love00242.vercel.app/#/articledetail?article=${articleNum}`);
     document
       .querySelector('meta[property="og:image"]')
       .setAttribute("content", `https://charity-love00242.vercel.app/meta_article${articleNum}.png`);
     document
       .querySelector('meta[property="og:title"]')
-      .setAttribute("content", articleList[articleNum- 1]);
+      .setAttribute("content", title);
+    document.title = title;
   } else {
     // document
     //   .querySelector('meta[property="og:url"]')
     //   .setAttribute("content", "https://event.udn.com/auroratrust/");
+    document
+      .querySelector('meta[property="og:url"]')
+      .setAttribute("content", "https://charity-love00242.vercel.app/");
     document
       .querySelector('meta[property="og:image"]')
       .setAttribute("content", "https://charity-love00242.vercel.app/meta_index.png");
     document
       .querySelector('meta[property="og:title"]')
       .setAttribute("content", "「平行公益　縫合大眾與助人者的認知落差」");
+    document.title = "「平行公益　縫合大眾與助人者的認知落差」";
   }
 })
 
